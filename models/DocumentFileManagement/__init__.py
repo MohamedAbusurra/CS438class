1e6ad8f3d13ef8fd391f9e7a1a75d711281493c1
sayf ammar <99840780+SayflyAM@users.noreply.github.com>
2025-05-10 23:23:56 +0200

    Create __init__.py

---
+++ models/DocumentFileManagement/__init__.py
@@ -0,0 +1,5 @@
+# DocumentFileManagement
+from models.DocumentFileManagement.file import File
+from models.DocumentFileManagement.file_version import FileVersion
+
+__all__  = ['File', 'FileVersion']
