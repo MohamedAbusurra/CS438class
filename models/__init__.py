8493482864fe2184c711c2c37c65675f62d0abb8
sayf ammar <99840780+SayflyAM@users.noreply.github.com>
2025-05-10 23:27:21 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,41 +1,12 @@
 from models.database import db, init_db
 from models.UserManagement.user import User
-from models.ProjectManagement.project import Project as projectModel
+from models.UserManagement.auth_token import AuthToken
+from models.ProjectManagement.project import Project
+from models.ProjectManagement.milestone import Milestone
 from models.DocumentFileManagement.file import File
-from models.DocumentFileManagement.file_version import FileVersion as file_version
+from models.DocumentFileManagement.file_version import FileVersion
 from models.TaskManagement.task import Task
 from models.AnalysisandReporting.report import Report
-"""
-so this the main init file for all model
-"""
-"""
-__all__ controls what gets import with "from models import *"
-"""
-__all__ =  [
-    'db' ,
-    'init_db' ,
-    'User' ,
-    'projectModel' ,
-    'File' ,
-    'file_version' ,
-    'Task' ,
-    'Report'
+from models.Communication.message import Message
 
-    ]
-"""
-jast make sure all models are I can use 
-them
-"""
-
-try :
-    all_models = [
-        User ,
-        projectModel ,
-        File ,
-        file_version  ,
-        Task  ,
-        Report
-    ]
-except ImportError as err:
-    raise(f'something broke while importing model {err}')
-
+__all__ = ['db', 'init_db', 'User', 'AuthToken', 'Project', 'Milestone', 'File', 'FileVersion', 'Task', 'Report', 'Message']
